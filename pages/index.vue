1b1f681ef2f4877fa86872026b9885e0ddbde624
Ben Mann <bnmann@ucdavis.edu>
2024-05-22 18:01:46 -0700

    css sucks

--- pages/index.vue
+++ pages/index.vue
@@ -28,18 +28,43 @@ const handleInput = (key) => { //handle on screen KB inputs (for now just loggin
   console.log("key",key);
 };
 
+const drawer = ref(null)
+
+</script>
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
 
 <template>
-  <div class="page">
-    <div class="kb">
-      <onScreenKB @onKeyPress="handleInput" />
-    </div>
-  </div>
+  <v-app id="template">
+    <v-navigation-drawer v-model="drawer">
+      <li><NuxtLink to="/">home</NuxtLink></li>
+      <li><NuxtLink to="/giver">giver</NuxtLink></li> <!-- MUTEX? LOCK? something.. -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      
+      <v-app-bar-title>Wordle With Friends!</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <div class="page">
+        <v-card class="text-center grey d-flex flex-column align-center justify-center" height="100%" maxWidth="700", maxHeight="500">
+          <onScreenKB @onKeyPress="handleInput" />
+        </v-card>
+      </div>
+    </v-main>
+  </v-app>
 
 </template>
 
 <style>
+.template{
+  background-color: grey;
+}
 .page{
   width: auto;
   height: 2000px;
@@ -50,14 +75,30 @@ const handleInput = (key) => { //handle on screen KB inputs (for now just loggin
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
+  max-width: 700px;
+  min-width: 500px;
+  max-height: 500px;
+  min-height: 500px;
 
   position:relative;
-  max-width: 700px;
   text-align: center;
   align-items: center;
-  justify-content: center;
   display: flex;
 
+  flex: 1;
+  justify-content: center;
+  align-items: "center";
+  background-color: "#ffc2c2";
+
+}
+.router-link-exact-active{
+  align-items: center;
+  color: rgb(27, 53, 167);
+  text-decoration: none;   /* underline */
+  font-weight: bold;     
+
+  margin: 0 8px;          /* Space between each link */
+  padding: 5 10px;      /* Padding around the link text */
 }
 </style>
 
